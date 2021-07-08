0c1c2f7a1e1f26e83c700db781c24707f98dc60c
gabizanellato <gabriellezanellato9@gmail.com>
2021-07-08 20:20:37 -0300

    criando pipe e revisando data binding

--- gerenciamento-cursos/src/app/app.module.ts
+++ gerenciamento-cursos/src/app/app.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
